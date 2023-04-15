721e9a44b680fd8c50dec61b4ca9ed91906d749f
ADA <you@example.com>
2023-04-15 18:19:17 +0600

    abylay

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
  console.log('App working...')
 
+setInterval(stopCheckingForUpdates, 400);
+
+ // setInterval() returns a value that we can use to stop the repeated
+ // invocations by calling clearInterval(). (Similarly, setTimeout()
+ // returns a value that you can pass to clearTimeout())
+ function stopCheckingForUpdates() {
+   if ($(window).width() < 960) {
+
+        var e1 = document.querySelector('.search__input');
+        e1.style.width = "500px";
+
+        var e1 = document.querySelector('.preview-zone');
+        e1.style.width = "600px";
+
+   }
+   else {
+     var e1 = document.querySelector('.search__input');
+     e1.style.width = "600px";
+     var e1 = document.querySelector('.preview-zone');
+     e1.style.width = "700px";
+   }
+
+   if ($(window).width() < 769) {
+
+        var e1 = document.querySelector('.search__form');
+        e1.style.display = "inline-block";
+
+        var e1 = document.querySelector('.preview-zone');
+        e1.style.width = "auto";
+
+        var e1 = document.querySelector('.search__input');
+        e1.style.width = "100%";
+
+        var e1 = document.querySelector('.search__btn-submit');
+        e1.style.width = "100%";
+
+        var e1 = document.querySelector('.probel');
+        e1.style.height = "5px";
+   }
+   else {
+     var e1 = document.querySelector('.search__form');
+     e1.style.display = "flex";
+
+     var e1 = document.querySelector('.preview-zone');
+     e1.style.width = "700px";
+
+     var e1 = document.querySelector('.search__input');
+     e1.style.width = "600px";
+
+     var e1 = document.querySelector('.search__btn-submit');
+     e1.style.width = "80px";
+
+     var e1 = document.querySelector('.probel');
+     e1.style.height = "0px";
+   }
+
+ };
+stopCheckingForUpdates();
+
+
 
  // выползание окошечка лог/рег
  var wrapper = document.getElementById('wrapper');
