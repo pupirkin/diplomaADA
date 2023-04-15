2f51fb8c9bc5713def292c08182ebe64a96f286b
pupirkin <pupirkin02@gmail.com>
2023-04-15 13:06:14 +0600

    seconds changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
  console.log('App working...')
+
+ var wrapper = document.getElementById('wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const bg = document.querySelector('.bg-modal');
+const pw = document.querySelector('.page_wrapper');
+
+console.log(document);
+const btnPopup = document.querySelector('.hih');
+const iconClose = document.querySelector('.icon-close');
+console.log(btnPopup);
+registerLink.addEventListener( 'click', ()=> {
+  wrapper.classList.add( 'active');
+});
+
+loginLink.addEventListener( 'click', ()=> {
+  wrapper.classList.remove( 'active');
+});
+
+btnPopup.addEventListener( 'click', ()=> {
+  console.log("hi");
+  wrapper.classList.add( 'active-popup');
+  bg.classList.add( 'active');
+  pw.classList.add( 'active');
+});
+
+iconClose.addEventListener( 'click', ()=> {
+  wrapper.classList.remove( 'active-popup');
+  bg.classList.remove( 'active');
+});
